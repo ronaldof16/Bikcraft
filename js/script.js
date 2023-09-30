333f04768936b390d7e4d95ddc27c42c5edf1e0a
ronaldof16 <ronaldof16@hotmail.com>
2023-09-30 15:21:06 -0300

    criação da função ativarLink

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-console.log("teste");
+let links = document.querySelectorAll(".header-menu a");
+
+function ativarLink(link) {
+    let url = location.href;
+    let href = link.href;
+
+    if(url.includes(href)) {
+        link.classList.add("ativo");
+    }
+}
+
+links.forEach(ativarLink);
