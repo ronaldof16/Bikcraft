4bbe14236ba316ce523f4aa92a559f4b295eed3f
ronaldof16 <ronaldof16@hotmail.com>
2023-09-30 16:09:25 -0300

    criação do script para ativar os itens do orçamento

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// Ativar links do menu 
+
 let links = document.querySelectorAll(".header-menu a");
 
 function ativarLink(link) {
@@ -10,3 +12,16 @@ function ativarLink(link) {
 }
 
 links.forEach(ativarLink);
+
+// Ativar itens do orçamento
+
+const parametro = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+    let elemento = document.getElementById(parametro);
+    if(elemento) {
+        elemento.checked = true;
+    }  
+}
+
+parametro.forEach(ativarProduto);
